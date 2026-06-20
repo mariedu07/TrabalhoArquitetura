096676c6084f875b6be8a49908ca605ff7c43f2f
mariedu <eduardamariamachado07@gmail.com>
2026-06-20 18:16:28 -0300

    Adiciona a primeita otimizacao do multiplicador de matrizes

--- otimizacao1.c
+++ otimizacao1.c
@@ -10,20 +10,20 @@
 
 /*SE QUISER FAZER MOSTRAR O TEMPO EM MS, TIRA A LINHA 18*/
 
-#define START_STOPWATCH( prm ) {gettimeofday( &prm.mStartTime, 0);} //relogio comeca a contar
+#define START_STOPWATCH( prm ) {gettimeofday( &prm.mStartTime, 0);} 
 
 #define STOP_STOPWATCH( prm ) {                                                        \
-  gettimeofday( &prm.mEndTime, 0); /*pega o horario exato q parou*/                                                    \
+  gettimeofday( &prm.mEndTime, 0);                                                 \
   prm.mElapsedTime = (1000.0f * ( prm.mEndTime.tv_sec - prm.mStartTime.tv_sec) + (0.001f * (prm.mEndTime.tv_usec - prm.mStartTime.tv_usec)) );  \
   prm.mElapsedTime /= 1000.0f;                                                         \
-}// relogio para de contar e ve quanto tempo se passou
+}
 
 typedef struct
 {
   struct timeval mStartTime;
   struct timeval mEndTime;
   double mElapsedTime;
-} STOPWATCH; //estrutura do cronometro
+} STOPWATCH; 
 
 #define EPSILON 1E-30
 #define ALING 64
@@ -33,38 +33,34 @@ typedef struct {
     double *vetor;
 }MATRIZ;
 
-//ASSINATURAS DAS FUNCOES
 
-size_t saveBin(MATRIZ *A, char *nomeArqv); //grava vet e matrizes no formato bin, *A estrutura de matriz que vai ser gravada em disco, nomeArqv nome do arquivobinario e return qtd bytes lidos
+size_t saveBin(MATRIZ *A, char *nomeArqv); 
+size_t loadBin(MATRIZ *A, char *nomeArqv);
 
-size_t loadBin(MATRIZ *A, char *nomeArqv); //size-t é um tipo especifico para tamanhos em c
-
-void multMatriz(MATRIZ *  __restrict__ C, //endereco da memoria da matriz resultado
-                  MATRIZ *  __restrict__ A, //endereco da memoria da matriz A
-                  MATRIZ *  __restrict__ B, //endereco da memoria da matriz B
-                  unsigned int nThreads){ // quantas threads vao ser usadas na multiplicacao
-
-     #pragma omp parallel // avisa ao compilador que o bloco dele deve ser executado de forma paralela
+void multMatrizOt1(MATRIZ *  __restrict__ C,
+                  MATRIZ *  __restrict__ A, 
+                  MATRIZ *  __restrict__ B, 
+                  unsigned int nThreads){
+     #pragma omp parallel
      {
-	#pragma omp for //abre o bloco
-        for (int j = 0; j < C->n; j++){
-            for (int i = 0; i < C->m; i++){
-                double c = 0.0f; //vai acumular a soma
-                for (int jA = 0; jA < A->m; jA++){ 
-                    int ak = j * A->m + jA;
-                    int bk = jA * B->m + i;
-                    c += A->vetor[ak] * B->vetor[bk];
+	#pragma omp for 
+        for (int i = 0; i < C->n; i++){
+            for (int k = 0; k A->m; k++){
+                    int ak = i * A->m + k;
+                    double numeroA = A->vetor[ak];
+
+                for (int j = 0; j < C->m; j++){
+                    int bk = k * B->m + i;
+                    int ck = i * c->m + j;
+                   
+                        C->vetor[ck] += numeroA * B->vetor[bk];    
                 }
-
-                int ck = j * C->m +  i;
-                C->vetor[ck] = c;
             }
-
         }
       }
 }
 
-// ./multi-mat-cpu.exec r2000x2000.bin i2000x2000.bin resultado.bin 4
+
 int main (int ac, char **av){
 
     MATRIZ A, B, C;
@@ -78,20 +74,19 @@ int main (int ac, char **av){
 
     unsigned int nThreads = atoi(av[4]);
     double contaBytes = 0.0,
-           *elapsedtime = NULL; //vetor para armazenar o tempo gasto por cada thread
+           *elapsedtime = NULL;
 
 
-    contaBytes  =  (double)loadBin(&A, nomeArqvA); //ve quantos bytes tem em a
-    contaBytes  += (double) loadBin(&B, nomeArqvB); // ve quantos bytes tem em a e b
+    contaBytes  =  (double)loadBin(&A, nomeArqvA); 
+    contaBytes  += (double) loadBin(&B, nomeArqvB); 
     C.m = A.n;
     C.n = B.m;
 
-    C.vetor = (double *) aligned_alloc(ALING, C.m *  C.n * sizeof(double)); // aloca a memoria para a matriz resultado, o tamanho é m*n e cada elemento é um double, o alinhamento é de 64 bytes
-    contaBytes  += (double) C.m *  C.n * sizeof(double); // ve quantos bytes tem em a, b e c
-    bzero(C.vetor, C.m *  C.n * sizeof(double)); // limpa o espaco q acabamos de alocar
+    C.vetor = (double *) aligned_alloc(ALING, C.m *  C.n * sizeof(double)); 
+    bzero(C.vetor, C.m *  C.n * sizeof(double)); 
 
 
-    elapsedtime = (double *) aligned_alloc(ALING, nThreads * sizeof(double)); //aloca a memoria para o vetor de tempo gasto por cada thread
+    elapsedtime = (double *) aligned_alloc(ALING, nThreads * sizeof(double)); 
     printf("\nMultiplicação de matrizes\n");
     printf("\t  Matriz A: %s \n", nomeArqvA);
     printf("\t  Matriz B: %s \n", nomeArqvB);
@@ -99,7 +94,7 @@ int main (int ac, char **av){
     printf("\t   Threads: %u \n", nThreads);
     printf("\t   Memória: %lf MBytes \n", ((contaBytes) / 1048576));
 
-    multiMatriz(&C, &A, &B, nThreads);
+    multiMatrizOt1 (&C, &A, &B, nThreads);
     saveBin(&C, nomeArqvC);
 
     printf("----------------------------------------------------------------\n");
@@ -136,7 +131,7 @@ size_t loadBin(MATRIZ *A, char *filename){
     assert(ptr != NULL);
     double *a = NULL;
     size_t bytes_read = 0, aux;
-    //numread = fread( list, sizeof( char ), 25, stream );
+
 
     aux = fread(&A->m, sizeof(A->m), 1, ptr); bytes_read += aux * sizeof(A->m);
     aux = fread(&A->n, sizeof(A->n), 1, ptr); bytes_read += aux * sizeof(A->n);
@@ -149,5 +144,5 @@ size_t loadBin(MATRIZ *A, char *filename){
     A->vetor = a;
     fclose(ptr);
     return bytes_read;
-    //print2Console(A);
+
 }
