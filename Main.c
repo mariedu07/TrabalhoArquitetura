9224126a9dc8c8be13b7b2b066377195f1ab0e9d
Mariedu07 <eduardamariamachado07@gmail.com>
2026-06-23 14:07:04 -0300

    primeiro commit

--- Main.c
+++ Main.c
@@ -1,16 +1,36 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-void gerarMatriz(float *Matriz, int tamMatriz){
+float* gerarMatriz(int tamMatriz){
+    float *Matriz = malloc(tamMatriz * tamMatriz * sizeof(float));
+
+    if (Matriz == NULL) {
+        printf("Erro de alocacao\n");
+        return NULL;
+    }
+
     for(int i = 0; i < tamMatriz; i++){
         for(int j = 0; j < tamMatriz; j++){
-            Matriz[i * tamMatriz + j] = rand() % 10; //%10
+            Matriz[i * tamMatriz + j] = rand() % 10;
         }
+    }
+
+    return Matriz;
+}
+
+void imprimirMatriz(float *Matriz, int tamMatriz){
+    for(int i = 0; i < tamMatriz; i++){
+        for(int j = 0; j < tamMatriz; j++){
+            printf("%.2f ", Matriz[i * tamMatriz + j]);
+        }
+        printf("\n");
     }
 }
 
 void zerarMatriz(float *Matriz, int tamMatriz){
-    for(int i = 0; i < tamMatriz * tamMatriz; i++)
-        Matriz[i] = 0.0f;
+    for(int i = 0; i < tamMatriz; i++)
+        for(int j = 0; j<tamMatriz; j++)
+             Matriz[i * tamMatriz +j] = 0.0f;
 }
 
 void multiplicacaoMatriz(float *matrizA, float *matrizB, float *matrizC, int tamMatriz){
@@ -25,4 +45,21 @@ void multiplicacaoMatriz(float *matrizA, float *matrizB, float *matrizC, int tam
     }
 }
 
+int main(){
+    int tamMatriz;
+    float *matrizA, *matrizB;
+    printf("Ordem das matrizes: ");
+    
+    scanf("%d",&tamMatriz);
+    matrizA = gerarMatriz(tamMatriz);
+    matrizB = gerarMatriz(tamMatriz);
+
+    printf("Matriz A gerada!\n");
+    imprimirMatriz(matrizA, tamMatriz);
+
+    printf("\n\nMatriz B gerada!\n");
+    imprimirMatriz(matrizB, tamMatriz);
+}
+
 //otimizacao transposta (recebe matriz A, matriz B transposta, e c q guarda, e tam matriz), faz um codigo pra transpor uma matriz
+//otimizacao omp
